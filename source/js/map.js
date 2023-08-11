1200b9e3d9bdb192a1141fb9e9bd7430545a98e1
Konstantin <mutecross@gmail.com>
2023-08-11 23:28:31 +0300

    добавлена карта

---
+++ source/js/map.js
@@ -0,0 +1,5 @@
+let mapPicture = document.querySelector('.location__map-picture');
+let mapIframe = document.querySelector('.location__map-iframe');
+
+mapPicture.classList.remove('location__map-picture--nojs');
+mapIframe.classList.remove('location__map-iframe--nojs');
